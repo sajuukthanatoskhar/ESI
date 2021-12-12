d172382e162874a69d6c2736b7152904c13c4c41
Sajuukthanatoskhar <gareth_willia64@hotmail.com>
2021-12-12 12:52:55 +0100

    ADD: market_watcher_keys as empty - for security reasons

---
+++ market_watcher_keys.py
@@ -0,0 +1,8 @@
+
+client_id = ""
+secret_key = ""
+call_back = "http://localhost:65432/callback/"
+scope = ["esi-markets.structure_markets.v1",
+         "esi-markets.read_character_orders.v1",
+         "esi-markets.read_corporation_orders.v1"]
+refresh_key: str = None
